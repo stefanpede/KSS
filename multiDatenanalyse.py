534db87908b83999cdb776bbe6febc46ba150b16
spede <stefanpede@gmx.de>
2017-01-25 16:00:25 +0100

    Interpretation of LDA

--- multiDatenanalyse.py
+++ multiDatenanalyse.py
@@ -1,7 +1,82 @@
 import numpy as np
 import pandas as pd
 import random
+from sklearn.preprocessing import LabelEncoder
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import matplotlib
 
+
+# high level functions
+def load_data(mmPfad='../data/Messmatrix.csv', cor_th=0.8):
+    
+    # Rohe Daten einladen
+    df = pd.read_csv(mmPfad)
+    
+    print("Rohe Daten: \n")
+    print("Anzahl der Kennwerte: "+str(df.shape[1]))
+    print("Anzahl der vermessenen Rohre: "+str(df.shape[0]))
+    print("Anzahl der gefahrenen Produkte: "+str(df.groupby(["Header_Leitguete","Header_Soll_AD","Header_Soll_WD"])["Header_Pseudonummer"].agg(["count"]).shape[0]))
+    print("Anzahl der Walzlose: "+str(len(pd.unique(df["Header_Walzlos"]))))
+    
+    # Vorberarbeitung, beseitigen von NaNs
+    dfVV2 = preprocess(df)
+    print("\nDaten nach Vorverarbeitung: \n")
+    print("Anzahl der Kennwerte: "+str(dfVV2.shape[1]))
+    print("Anzahl der vermessenen Rohre: "+str(dfVV2.shape[0]))
+    print("Anzahl der gefahrenen Produkte: "+str(dfVV2.groupby(["Header_Leitguete","Header_Soll_AD","Header_Soll_WD"])["Header_Pseudonummer"].agg(["count"]).shape[0]))
+    print("Anzahl der Walzlose: "+str(len(pd.unique(dfVV2["Header_Walzlos"]))))
+    
+    # Korrelierte Merkmale entfernen
+    print("\nKorrelierte Merkmale entfernen\n")
+    dfNoCor, _ = dropCorrelatedColumns((dfVV2[dfVV2.columns[6:]], dfVV2[dfVV2.columns[6:]]), cor_th)
+    dfVV2 = pd.concat((dfVV2[dfVV2.columns[:6]], dfNoCor), axis=1)
+    
+    return dfVV2
+
+def extract_product(df, product_id=0, min_num_walzlos=100):
+    # get product
+    groupby_list, product = get_product(df, product_id)
+    df_prod = df.query(" & ".join(["({} == {})".format(name, param) for name, param in zip(groupby_list, product)]))
+    
+    df_walzlos = df_prod.groupby(["Header_Walzlos"])["Header_Pseudonummer"].agg(["count"])
+    
+    # drop 
+    walzlose_to_drop = df_walzlos[(df_walzlos['count'] < min_num_walzlos)].index.tolist()
+    for walzlos in walzlose_to_drop:
+        df_prod.drop(df_prod[df_prod["Header_Walzlos"] == walzlos].index, inplace=True)
+        
+    return df_prod
+
+def plot_lda(X_lda, y, title, ax=None):
+    if ax:
+        ax = ax
+    else:
+        fig = plt.figure(figsize=(10,5))
+        ax = fig.add_subplot(111, projection='3d')
+        
+    for color, label in enumerate(np.unique(y)):
+        min_val = 0
+        max_val = len(np.unique(y))
+
+        my_cmap = plt.cm.get_cmap('rainbow') # or any other one
+        norm = matplotlib.colors.Normalize(min_val, max_val) # the color maps work for [0, 1]
+
+        color_i = my_cmap(norm(color)) # returns an rgba value
+
+        ax.scatter(X_lda[:,0][y==label], X_lda[:,1][y==label], X_lda[:,2][y==label], marker='*', color=color_i,
+                    label=label, alpha=1)
+        
+    ax.set_xlabel('LDA_1')
+    ax.set_ylabel('LDA_2')
+    ax.set_zlabel('LDA_3')
+    ax.set_title(title)
+    
+    #ax.legend()
+    ax.grid()                   
+    
+
+# basic functions
 def dropCorrelatedColumns(sets, covBound):
     from scipy import stats
 
@@ -17,7 +92,7 @@ def dropCorrelatedColumns(sets, covBound):
     for a,b in zip(np.where(kovBool)[0], np.where(kovBool)[1]):
         if (a != b):
             korr.append([a,b])
-    print(korr)
+            
     ## sortiere diese und finde einzigartige
     korr = [sorted(i) for i in korr]
     korrWD = []
@@ -58,7 +133,8 @@ def get_product(df, index):
     
     return groupby_list, products[index]
 
-def get_data(df, label_encoder, test_frac=0.2):
+def get_lda_data(df, test_frac=0.2):
+    label_encoder = LabelEncoder().fit(df["Header_Walzlos"])
     train_set = {'data': [], 'label': []}
     test_set = {'data': [], 'label': []}
 
@@ -90,14 +166,3 @@ def get_data(df, label_encoder, test_frac=0.2):
     test_set['data'] = np.asarray(test_set['data'])
         
     return train_set, test_set
-
-def zscore(data, mean=None, std=None):
-    if mean is None:
-        mean = np.mean(data, axis=0, keepdims=True)
-    
-    if std is None:
-        std = np.std(data, axis=0, keepdims=True)
-        
-    normed_data = (data-mean)/std
-    
-    return normed_data, mean, std
